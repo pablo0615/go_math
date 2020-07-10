b35dab2b25ec3cd3e17c17e2d4178ece58e76095
pablo0615 <paul@asgteach.com>
2020-07-10 18:20:27 +0000

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pablo0615/go_math
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,10 @@
+// main.go - Main program - version 1
+package go_math
+
+func Add(a, b int) int { return a + b }
+
+func Sub(a, b int) int { return a - b }
+
+func Mul(a, b int) int { return a * b }
+
+func Div(a, b int) int { return a / b }
